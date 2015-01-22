b83b0f32a15b07902ca43c3bcf79e6542b76d20b
Jan Hudecek <j.hudecek@nki.nl>
2015-01-22 17:16:27 +0100

    Added docs

--- src/main/groovy/org/transmartproject/core/dataquery/highdim/tworegion/GenesInEventValues.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/highdim/tworegion/GenesInEventValues.groovy
@@ -5,7 +5,13 @@ package org.transmartproject.core.dataquery.highdim.tworegion
  */
 interface GenesInEventValues {
     long getId()
+    /**
+     * HUGO gene identifier
+     */
     String getGeneId()
+    /**
+     * effect of the event on the gene: FUSION, CONTAINED, DISRUPTED, ...  (from CGA)
+     */
     String getEffect()
     long getEventId()
 }
--- src/main/groovy/org/transmartproject/core/dataquery/highdim/tworegion/JunctionInEventValues.groovy
+++ src/main/groovy/org/transmartproject/core/dataquery/highdim/tworegion/JunctionInEventValues.groovy
@@ -5,11 +5,32 @@ package org.transmartproject.core.dataquery.highdim.tworegion
  */
 interface JunctionInEventValues {
     JunctionValues getJunction()
+    /**
+     * number of reads in the whole span
+     */
     int getReadsSpan()
+    /**
+     * number of reads spanning the junction
+     */
     int getReadsJunction()
+    /**
+     * number of spanning mate pairs
+     */
     int getPairsSpan()
+    /**
+     * number of spanning mate pairs where one end spans a fusion
+     */
     int getPairsJunction()
+    /**
+     * number of mate pairs that support the fusion and whose one end spans the fusion
+     */
     int getPairsEnd()
+    /**
+     * number of reads that contradict the fusion by mapping to only one of the chromosomes
+     */
     int getPairsCounter()
+    /**
+     * frequency in the baseline set of genomes for the junction
+     */
     double getBaseFreq()
 }
